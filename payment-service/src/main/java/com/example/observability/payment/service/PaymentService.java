64aaa5265dc9325422b905a5252ac523e2e01a71
yashodhah <y.dominatz@gmail.com>
2024-06-02 14:38:13 +0800

    observation api : sample

--- payment-service/src/main/java/com/example/observability/payment/service/PaymentService.java
+++ payment-service/src/main/java/com/example/observability/payment/service/PaymentService.java
@@ -1,6 +1,7 @@
 package com.example.observability.payment.service;
 
 import com.example.observability.payment.model.OrderPayment;
+import io.micrometer.observation.annotation.Observed;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +9,27 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class PaymentService {
 
+    @Observed(name = "payment:processPayment")
     public void processPayment(OrderPayment orderPayment) {
-        log.info("Processing payment: {}", orderPayment.id());
+        log.info("Starting payment processing for Order ID: {}", orderPayment.id());
+
+        try {
+            log.info("Validating payment details for Order ID: {}", orderPayment.id());
+            Thread.sleep(500);
+            log.info("Payment details validated for Order ID: {}", orderPayment.id());
+            log.info("Authorizing payment for Order ID: {}", orderPayment.id());
+            Thread.sleep(500);
+            log.info("Payment authorized for Order ID: {}", orderPayment.id());
+            log.info("Capturing payment for Order ID: {}", orderPayment.id());
+            Thread.sleep(500);
+            log.info("Payment captured for Order ID: {}", orderPayment.id());
+            log.info("Completing payment processing for Order ID: {}", orderPayment.id());
+            Thread.sleep(500);
+            log.info("Payment processing completed for Order ID: {}", orderPayment.id());
+        } catch (InterruptedException e) {
+            log.error("Thread was interrupted while processing payment for Order ID: {}", orderPayment.id(), e);
+        } catch (Exception e) {
+            log.error("Exception occurred while processing payment for Order ID: {}", orderPayment.id(), e);
+        }
     }
 }
